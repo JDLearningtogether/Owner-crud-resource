670e7a896d107d2f15f28bb0ec4aad9fb2b4a3bc
Chanida Sutchrith <jdlearningtogether@gmail.com>
2026-08-27 10:30:40 +0700

    edit_owner

--- resources/views/owners/create.blade.php
+++ resources/views/owners/create.blade.php
@@ -5,7 +5,7 @@
 <div class="container">
 
     <h3 class="mb-4">
-        เพิ่มเจ้าของกิจการ
+        เพิ่มเจ้าของกิจการ : รายละเอียด
     </h3>
 
     <form action="{{ route('owners.store') }}" method="POST">
